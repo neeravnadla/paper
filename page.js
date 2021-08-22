cec1756122aba2d8ce381c118f34c522c6dfa0c8
neenadla <neeravnadla@gmail.com>
2021-08-22 19:07:54 +0530

    ...

--- page.js
+++ page.js
@@ -13,56 +13,67 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
-document.querySelector("#clear").addEventListener("click", ()=>{
 
+// clear text button
+
+document.querySelector("#clear").addEventListener("click", ()=>{
     document.querySelector("#newmsg").value = "";
 });     
 });
 
 
+// text resize button
 
-document.querySelector("#fontplush").addEventListener("click", ()=>{
-
-    fontsize("plus")
-
-});     
-
-document.querySelector("#fontminus").addEventListener("click", ()=>{
-
-    fontsize("minus")
-
-});   
+document.querySelector("#fontplush").addEventListener("click", ()=>{fontsize("plus")});     
+document.querySelector("#fontminus").addEventListener("click", ()=>{fontsize("minus")});   
 
 function fontsize(button){
 
     if (button === "plus"){
 
-        let p = document.querySelector("#fontplush");
-     let m = document.querySelector("#fontminus");
-
+    let p = document.querySelector("#fontplush");
+    let m = document.querySelector("#fontminus");
     let s = Number(p.dataset.fs) + 2;
-
     document.querySelector("#newmsg").style.fontSize = s+"px";
-
     p.dataset.fs = s;
     m.dataset.fs = s;
-
     }
     
     if(button === "minus"){
 
-        
     let p = document.querySelector("#fontplush");
     let m = document.querySelector("#fontminus");
-
     let s = Number(p.dataset.fs) - 2;
-
     document.querySelector("#newmsg").style.fontSize = s+"px";
-
     p.dataset.fs = s;
     m.dataset.fs = s;
-
     }
+}
 
-    
+// font color button action
+
+var colorWell;
+var defaultColor = "#666";
+
+window.addEventListener("load", startup, false);
+function startup() {
+
+  colorWell = document.querySelector("#fontcolor");
+  colorWell.value = defaultColor;
+  colorWell.addEventListener("input", updateFirst, false);
+  
+}
+
+function updateFirst(event) {
+  var p = document.querySelector("#newmsg");
+
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+function watchColorPicker(event) {
+  document.querySelectorAll("#newmsg").forEach(function(p) {
+    p.style.color = event.target.value;
+  });
 }
