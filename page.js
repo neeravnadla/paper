b592b385eb38a3aac96ac5daef6b53cac0e66018
neenadla <neeravnadla@gmail.com>
2021-09-07 13:41:20 +0530

    save as image added

--- page.js
+++ page.js
@@ -3,82 +3,84 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    // load channel buttons    
-    document.querySelectorAll(".coloroption").forEach(element => {
+  // load channel buttons    
+  document.querySelectorAll(".coloroption").forEach(element => {
     let e = element;
     element.style.backgroundColor = element.dataset.dcolor;
-    e.onclick = ()=>{
-        let eid = document.getElementById(e.id).dataset.dcolor;  ;
-        document.querySelector(".addnew").style.backgroundColor = eid;       
-        document.querySelector("#newmsg").style.backgroundColor = eid;
-    } ; 
-});
+    e.onclick = () => {
+      let eid = document.getElementById(e.id).dataset.dcolor;;
+      document.querySelector(".addnew").style.backgroundColor = eid;
+      document.querySelector("#newmsg").style.backgroundColor = eid;
+    };
+  });
 
 
-// paper color button action
+  // paper color button action
 
-var colorPage;
-var defaultPaper = "#666";
+  var colorPage;
+  var defaultPaper = "#666";
 
-window.addEventListener("load", startup, false);
-function startup() {
+  window.addEventListener("load", startup, false);
+  function startup() {
 
-  colorPage = document.querySelector("#papercolor");
-  colorPage.value = defaultPaper;
-  colorPage.addEventListener("input", update, false);
-  
-}
+    colorPage = document.querySelector("#papercolor");
+    colorPage.value = defaultPaper;
+    colorPage.addEventListener("input", update, false);
 
-function update(event) {
-   var b = document.querySelector(".addnew");
-  var p = document.querySelector("#newmsg");
- 
-
-  if (p) {
-    p.style.backgroundColor = event.target.value;
-    b.style.backgroundColor = event.target.value;
   }
-}
+
+  function update(event) {
+    var b = document.querySelector(".addnew");
+    var p = document.querySelector("#newmsg");
+
+
+    if (p) {
+      p.style.backgroundColor = event.target.value;
+      b.style.backgroundColor = event.target.value;
+    }
+  }
 
 
 
 
 
 
-// clear text button
+  // clear text button
 
-document.querySelector("#clear").addEventListener("click", ()=>{
+  document.querySelector("#clear").addEventListener("click", () => {
     document.querySelector("#newmsg").value = "";
-});     
+    document.querySelector("#newmsg").style.height = "60vh";
+
+  });
 });
 
 
 // text resize button
 
-document.querySelector("#fontplush").addEventListener("click", ()=>{fontsize("plus")});     
-document.querySelector("#fontminus").addEventListener("click", ()=>{fontsize("minus")});   
+document.querySelector("#fontplush").addEventListener("click", () => { fontsize("plus") });
+document.querySelector("#fontminus").addEventListener("click", () => { fontsize("minus") });
 
-function fontsize(button){
+function fontsize(button) {
 
-    if (button === "plus"){
+  if (button === "plus") {
 
     let p = document.querySelector("#fontplush");
     let m = document.querySelector("#fontminus");
     let s = Number(p.dataset.fs) + 2;
-    document.querySelector("#newmsg").style.fontSize = s+"px";
+    document.querySelector("#newmsg").style.fontSize = s + "px";
     p.dataset.fs = s;
     m.dataset.fs = s;
-    }
-    
-    if(button === "minus"){
+  }
+
+  if (button === "minus") {
 
     let p = document.querySelector("#fontplush");
     let m = document.querySelector("#fontminus");
     let s = Number(p.dataset.fs) - 2;
-    document.querySelector("#newmsg").style.fontSize = s+"px";
+    document.querySelector("#newmsg").style.fontSize = s + "px";
     p.dataset.fs = s;
     m.dataset.fs = s;
-    }
+  }
 }
 
 // font color button action
@@ -92,7 +94,7 @@ function startup() {
   colorWell = document.querySelector("#fontcolor");
   colorWell.value = defaultColor;
   colorWell.addEventListener("input", updateFirst, false);
-  
+
 }
 
 function updateFirst(event) {
@@ -104,13 +106,61 @@ function updateFirst(event) {
 }
 
 
-document.querySelector("#copy").addEventListener('click',()=>{
-   let copyText= document.querySelector("#newmsg").value;
-    if(copyText===""){
-      alert("paper is empty");
-    }
-    else{
+// copy text from paper 
+
+document.querySelector("#copy").addEventListener('click', () => {
+  let copyText = document.querySelector("#newmsg").value;
+  if (copyText === "") {
+    alert("paper is empty");
+  }
+  else {
     navigator.clipboard.writeText(copyText);
     alert("Text Copied");
-    }
+  }
 });
+
+// save as image 
+
+document.querySelector("#shot").addEventListener('click', () => {
+  var node = document.querySelector('.addnew');
+
+  domtoimage.toPng(node)
+    .then(function (dataUrl) {
+
+      download(dataUrl);
+
+    })
+    .catch(function (error) {
+      console.error('oops, something went wrong!', error);
+    });
+
+
+
+});
+
+
+
+
+// download pic
+
+function download(s) {
+  const link = document.createElement('a');
+  link.href = s;
+  link.setAttribute('download', 'image.jpg');
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+}
+
+
+
+// resize page
+
+
+textarea = document.querySelector("#newmsg");
+textarea.addEventListener('input', autoResize, false);
+
+function autoResize() {
+
+  this.style.height = this.scrollHeight + 'px';
+}
